aaa386c9ec4edf9e09ae62c56fb8902a7de8e95e
liangyanxiang <liangyanxiang@liangyanxiangdeMacBook-Pro.local>
2019-01-24 09:40:45 +0800

    update

--- app/root.js
+++ app/root.js
@@ -1,29 +1,44 @@
 
-import React, { Component } from 'react';
+import React from 'react';
+import { Provider } from 'react-redux';
+import App from './containers/app'
+console.log(App)
+
 import {
-  StyleSheet,
-  Text,
-  View
+    StyleSheet,
+    Text,
+    View
 } from 'react-native';
 
-export default class Root extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+const Root = () => {
+    <Provider store={{}}>
+        <p>9999999</p>
+
+    </Provider>
 }
+
+export default Root;
+
+import  { Component } from 'react';
+
+// export default class app extends Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text style={styles.welcome}>
+//           Welcome to React Native!
+//         </Text>
+//         <Text style={styles.instructions}>
+//           To get started, edit index.ios.js
+//         </Text>
+//         <Text style={styles.instructions}>
+//           Press Cmd+R to reload,{'\n'}
+//           Cmd+D or shake for dev menu
+//         </Text>
+//       </View>
+//     );
+//   }
+// }
 
 const styles = StyleSheet.create({
   container: {
